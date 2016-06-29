bd181e2d46ae05bc5d07f6728361b07100d0b420
Jasper O'Leary <jhaazpr@gmail.com>
2016-06-28 17:36:28 -0700

    [WIP] attempted to add a route to setup the arduino server
    but it does not work :( for now, the user must start their own serial server

--- rails-iot/app/controllers/tool_controller.rb
+++ rails-iot/app/controllers/tool_controller.rb
@@ -9,7 +9,13 @@ class ToolController < ApplicationController
     render :json => @files
   end
 
-  # HELPER METHODS
+ def start_server
+   # NOTE: currently doesn't work :( 
+   dir = system('ruby ./ruby_scripts/ArduinoServer.rb &')
+   render :json => {msg: "I started server", debug: dir}
+ end
+
+ # HELPER METHODS
   def get_displays
     files = {path: "/artwork/", filenames: Dir.glob("public/artwork/*.svg").collect!{|c| c.split('/')[2..-1].join('/')}}
     files[:filenames].collect!{|f| {:collection => f.split('.')[0].split('-')[0].split('_')[0].titlecase, :filename => f, :title => f.split(".")[0].titlecase}}
