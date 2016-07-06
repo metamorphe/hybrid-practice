f54706237b5873f1e19d9aceb82f875dab9a0ce4
Cesar Torres <cearto@berkeley.edu>
2016-07-06 09:48:44 -0700

    components

--- rails-iot/app/assets/javascripts/eld/ButtonExporter.js
+++ rails-iot/app/assets/javascripts/eld/ButtonExporter.js
@@ -12,12 +12,13 @@ function ButtonExporter(dom, type, preFN, postFN){
       var scope = this;
       if(this.type == "PNG")
         $(this.dom).click(function(){
-          display = new Artwork("/artwork/nine-segment.svg", function(artwork){
-            scope.preFN(artwork);
-            var fn = scope.getFilename();
-            ButtonExporter.exportPNG(fn, scope.dom);
-            paper.project.clear();
-            paper.view.update();
+          display = new Artwork("/artwork/sun_moon.svg", function(artwork){
+            result = scope.preFN(artwork);
+                      
+            // var fn = scope.getFilename();
+            // ButtonExporter.exportPNG(result, fn, scope.dom);
+            // paper.project.clear();
+            // paper.view.update();
           })
         })
       else if(this.type == "SVG")
@@ -37,10 +38,17 @@ function ButtonExporter(dom, type, preFN, postFN){
   }
 
 
-  ButtonExporter.exportPNG = function(filename, dom){
+  ButtonExporter.exportPNG = function(result, filename, dom){
       console.log("Exporting PNG...", filename);
-      dom.attr('href', $('#myCanvas')[0].toDataURL("image/png"))
-             .attr('download', filename + '.png');
+
+      result.position =  paper.project.view.projectToView(new paper.Point(result.strokeBounds.width / 2.0, result.strokeBounds.height / 2.0));
+      paper.view.update();
+      bufferCanvas = copyCanvasRegionToBuffer($('#myCanvas')[0], 0, 0, result.strokeBounds.width * 2, result.strokeBounds.height * 2);
+      dom.attr('href', bufferCanvas.toDataURL("image/png"))
+      .attr('download', filename + '.png');
+
+      // dom.attr('href', $('#myCanvas')[0].toDataURL("image/png"))
+             // .attr('download', filename + '.png');
   }
 
   ButtonExporter.exportSVG = function(filename){
@@ -58,3 +66,11 @@ function ButtonExporter(dom, type, preFN, postFN){
     paper.view.zoom = prev_zoom;
     paper.view.update();
   }
+
+function copyCanvasRegionToBuffer( canvas, x, y, w, h, bufferCanvas ){
+  if (!bufferCanvas) bufferCanvas = document.createElement('canvas');
+  bufferCanvas.width  = w;
+  bufferCanvas.height = h;
+  bufferCanvas.getContext('2d').drawImage( canvas, x, y, w, h, 0, 0, w, h );
+  return bufferCanvas;
+}
