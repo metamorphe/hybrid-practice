f61882b57c720f167f91b4f0c811664d436e43f0
Cesar Torres <cearto@berkeley.edu>
2016-08-16 16:47:00 -0700

    add led

--- rails-iot/app/assets/javascripts/eld/brushes/LEDPlacerBrush.js
+++ rails-iot/app/assets/javascripts/eld/brushes/LEDPlacerBrush.js
@@ -9,10 +9,28 @@ function LEDPlacerBrush(paper){
 	this.paper = paper;
 	this.name = "LEDPlacerBrush";
 	this.tool = new paper.Tool();
+	this.hue = 0; 
+	this.hue_step = (360 /8);
 	var scope = this;
 	this.selection = [];
 
 	this.tool.onMouseDown = function(event){
+		var diffs = CanvasUtil.queryPrefix('DIF');
+
+		if(event.event.metaKey){
+
+			var led = new paper.Path.Rectangle({
+				name: "NLED: APA102C", 
+				size: new paper.Size(Ruler.mm2pts(LED_WIDTH), Ruler.mm2pts(LED_WIDTH)),
+				position: event.point, 
+				fillColor: "white", 
+				parent: CanvasUtil.queryPrefix("ELD")[0]
+			});
+
+			scope.addRays(diffs, led);
+			scope.selection.push(led.id);
+			return;
+		}
 		var hitResult = project.hitTest(event.point, hitOptions);
 		if (!hitResult)
 			return;
@@ -66,6 +84,11 @@ function LEDPlacerBrush(paper){
 LEDPlacerBrush.prototype = {
 	enable: function(){
 	   var scope = this;
+	   diffs = CanvasUtil.queryPrefix("DIF");
+	   leds = CanvasUtil.queryPrefix("NLED");
+	   _.each(leds, function(led){
+	   		scope.addRays(diffs, led);
+	   });
 	},
 	disable: function(){
 	   var scope  = this;
@@ -75,6 +98,67 @@ LEDPlacerBrush.prototype = {
 	}, 
 	clear: function(){
 	
+	}, 
+	addRays: function(diffs, led){
+		var scope = this;
+		var diffs = _.filter(diffs, function(diff){ return diff.contains(led.position);});
+		if(diffs.length ==  0) return;
+				
+		var rays = CanvasUtil.query(paper.project, { prefix: "RAY", originLight: led.id });
+		if(rays.length != 0){
+			CanvasUtil.set(rays, "position", led.position.clone());
+		} else{
+			// CREATE RAYS
+			var rays = _.range(-180, 180, 1);
+			var color = new paper.Color("red");
+
+			scope.hue = (scope.hue + scope.hue_step) % 360;
+			color.hue = scope.hue;
+
+			color.saturation = 0.8;
+			color.brightness = 0.8;
+
+			var led_color = color.clone();
+			led_color.brightness = 1;
+			led.set({
+				fillColor: led_color, 
+				strokeColor: led_color, 
+				strokeWidth: 1
+			})
+
+			rays = _.map(rays, function(theta){
+				var point = new paper.Point(1, 0);
+				point.length = Ruler.mm2pts(MAX_APA102C_RAY_LENGTH);
+				point.angle = theta;
+				var line = new paper.Path.Line({
+					name: "RAY: Cast",
+					from: led.position.clone(), 
+					to: led.position.clone().add(point), 
+					strokeColor: color, 
+					strokeWidth: 1,
+					opacity: 0.2, 
+					parent: CanvasUtil.queryPrefix("ELD")[0], 
+					originLight: led.id, 
+					originAngle: theta,
+					hue: scope.hue, 
+					// applyMatrix: false
+					// shadowColor: color,
+					// shadowBlur: 30,
+				  // shadowOffset: new Point(1, 1)
+				});
+				line.pivot = line.firstSegment.point.clone();;
+				
+				ixts = line.getIntersections(diffs[0]);
+				if(ixts.length > 0)
+				line.lastSegment.point = ixts[0].point;
+			});
+			
+		}
+		// UPDATE
+		leds = CanvasUtil.queryPrefix("NLED");
+		   _.each(leds, function(led, i){
+				led.bringToFront();
+		   });
 	}
 }
 
