9e05ef384f324d29c29738fd201023211d32ca7c
Cesar Torres <cearto@berkeley.edu>
2016-10-10 12:50:14 -0700

    demo shneider

--- rails-iot/ruby_scripts/ArduinoServer.rb
+++ rails-iot/ruby_scripts/ArduinoServer.rb
@@ -2,8 +2,8 @@ require 'em-websocket'
 require 'json'
 require 'serialport'
 
-# sp = SerialPort.new('/dev/tty.usbmodem1421', 19200 * 6, 8, 1, SerialPort::NONE)
-sp = SerialPort.new('/dev/tty.usbmodem1411', 9600, 8, 1, SerialPort::NONE)
+sp = SerialPort.new('/dev/tty.usbmodem1421', 9600, 8, 1, SerialPort::NONE)
+# sp = SerialPort.new('/dev/tty.usbmodem1411', 9600, 8, 1, SerialPort::NONE)
 print "Starting server at 3015\n"
 
 def message_from(sp)
@@ -18,7 +18,7 @@ EventMachine::WebSocket.start(:host => '0.0.0.0', :port => 3015) do |ws|
     # ws.send msg
     print "MSGFA:" + msg + "\n"
   }
-  ws.onclose   { puts "WebSocket closed" }
+  # ws.onclose   { puts "WebSocket closed" }
   ws.onmessage do |msg|
       print "MSGFS:" + msg + "\n"
       sp.write(msg);
