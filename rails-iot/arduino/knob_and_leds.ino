aada9eef81bf3b73426d37078095d06a008ef858
Cesar Torres <cearto@berkeley.edu>
2017-03-07 10:00:28 -0800

    non-blocking state and sensing routine

--- rails-iot/arduino/knob_and_leds.ino
+++ rails-iot/arduino/knob_and_leds.ino
@@ -2,36 +2,52 @@
  *  Updated 31 October 2016
  *  Author: Cesar Torres
  */
-
-#define NUMPIXELS 16 // Number of LEDs in strip
+ 
+// SENSORS AND ACTUATOR PINS
 #define LED1_PIN 9
 #define LED2_PIN 10 
 #define LED3_PIN 11 
 #define POT_PIN A0 
 int leds[] = {9, 10, 11};
-uint16_t id = 0;
-uint8_t r = 0;
-uint8_t g = 0;
-uint8_t b = 0;
+
+void registerActuators(){
+  // add all setup commands for actuators here
+  pinMode(LED1_PIN, OUTPUT);
+  pinMode(LED2_PIN, OUTPUT);
+  pinMode(LED3_PIN, OUTPUT);
+}
+
+void registerSensors(){
+  // add all setup commands for actuators here
+  pinMode(POT_PIN, INPUT);
+}
+
+// API PROCESSING
+char prefix = 0;
 char buffer = ' ';
+uint16_t id = 0;
+uint8_t b = 0;
+unsigned long startTime = 0;
+unsigned long elapsedTime = 0;
+
+
+// STATE MACHINE
 char state = ' ';
-bool off = false;
+char prevState = ' ';
+bool off = false; // ARTIFICIAL TURN OFF (UNBINDS API)
 
 /* SANDBOX API */
-char prefix = 0;
-void findCommandEnd(){
-  while(Serial.available() > 0){
-    buffer = Serial.read();
-    if(buffer == '\n') break;
-  }
-}
 void color_change(){
   id = Serial.parseInt();
   b = Serial.parseInt();
   Serial.println(id);
   Serial.println(b);
   analogWrite(leds[id], b);
-//  strip.show();
+}
+
+void findCommandEnd(){
+  buffer = ' ';
+  while(Serial.available() > 0 && buffer != '\n') buffer = Serial.read();
 }
 
 void api_call(char prefix){
@@ -55,42 +71,50 @@ void api_call(char prefix){
   }
 }
 
-
-
-// SERIAL CONSOLE INTERFACE
-void registerActuators(){
-  // add all setup commands for actuators here
-  pinMode(LED1_PIN, OUTPUT);
-  pinMode(LED2_PIN, OUTPUT);
-  pinMode(LED3_PIN, OUTPUT);
-}
-
-void registerSensors(){
-  // add all setup commands for actuators here
-  pinMode(POT_PIN, INPUT);
-}
-
-
 void setup() {
   Serial.begin(9600);
   Serial.println("Aesthetic Actuation Controller");
   registerActuators();
   registerSensors();
-  good_state();
   delay(3000);
+  startTime = millis();
 }
 
 void enable_api(){
-   // Listening for character input for Serial console
   if (Serial.available() > 0) {
-      // read the incoming byte:
        prefix = Serial.read();
-       Serial.print("Calling API Command: ");
-       Serial.print(prefix);
-       Serial.print(" ");
        api_call(prefix);
   }
-//  delay(50);              // wait for a 50 milliseconds
+}
+void update(){
+  if(state == 'b') return;
+  if(prevState == state) return;
+  Serial.print("S:");
+  switch(state){
+    case 'g':
+       Serial.print("good");
+       break;
+    case 'f':
+       Serial.print("fast");
+       break;
+    case 'e':
+       Serial.print("error");
+       break;
+    case 'x':
+       Serial.print("failure");
+       break;
+    case 's':
+       Serial.print("success");
+       break;
+    case 'o':
+       Serial.print("on");
+       break;
+    case 'd':
+       Serial.print("off");
+       break;
+  }
+  Serial.println();
+  prevState = state;
 }
 
 int val = 0; 
@@ -98,103 +122,61 @@ int old_val = 0;
 int delta = 0;
 int strikes = 3;
 
-
-
-// BEGIN STATES
-
-void good_state(){
-  if(state == 'g') return;
-  state = 'g';
-  Serial.print("S:");
-  Serial.print("good");
-  Serial.println();
-}
-void fast_state(){
-  if(state == 'f') return;
-  state = 'f';
-  Serial.print("S:");
-  Serial.print("fast");
-  Serial.println();
-}
-void error_state(){
-  if(state == 'e') return;
-  state = 'e';
-  Serial.print("S:");
-  Serial.print("error");
-  Serial.println();
-}
-void failure_state(){
-  if(state == 'x') return;
-  state = 'x';
-  Serial.print("S:");
-  Serial.print("failure");
-  Serial.println();
-}
-void success_state(){
-  if(state == 's') return;
-  state = 's';
-  Serial.print("S:");
-  Serial.print("success");
-  Serial.println();
-}
-void on_state(){
-  if(state == 'o') return;
-  state = 'o';
-  Serial.print("S:");
-  Serial.print("on");
-  Serial.println();
-}
-void off_state(){
-  if(state == 'd') return;
-  state = 'd';
-  Serial.print("S:");
-  Serial.print("off");
-  Serial.println();
-  off = true;
-}
-
-// END STATES
-
 void state_machine(int val, int delta){
   if(delta <= 2)
-    good_state();
+    state = 'g';
   else if(delta > 2 && delta <= 4)
-    fast_state();
-  else if(delta > 4 && strikes > 0){
-    error_state();
+    state = 'f';
+  else if(delta > 5 && strikes > 0){
+    state = 'e';
     strikes--;
   }
-  else if(delta > 6){
-    failure_state();
-    delay(3000);
-    off_state();
+  else if(delta > 8){
+    state = 'x';
+    non_block_delay('o', 3000);
+    off = true;
   }
   if(strikes <= 0){
-    failure_state();
-    delay(3000);
-    off_state();
+    state = 'x';
+    non_block_delay('o', 3000);
+    off = true;
   }
   if(val < 5 && strikes > 0){
-    success_state();
-    delay(3000);
-    off_state();
-  }  
+    state = 's';
+    non_block_delay('o', 3000);
+    off = true;
+  }
+  update();  
 }
 
-void calm_turn(){
+void non_block_delay(char state, int delay){
+  state = 'b';
+  Serial.print("T:");
+  Serial.print(state);
+  Serial.print(",");
+  Serial.print(delay);
+  Serial.println();
+}
+
+void sense(){
   val = analogRead(POT_PIN);
   val = map(val, 0, 1024, 0, 100);
-  delta = old_val - val;
-  old_val = val;
-  state_machine(val, delta);
- 
+  
+  if(elapsedTime > 200){
+    state_machine(val, delta);
+    delta = 0;
+    startTime = millis();
+  }else{
+    delta = delta + (old_val - val);
+    old_val = val;
+  }
 }
 
 void loop() {
+  elapsedTime = millis() - startTime;
   if(!off){
-    calm_turn();
-    enable_api();
-    delay(200);
+    sense(); // artificial turn_off
   }
+  enable_api(); // always on
 }
 
